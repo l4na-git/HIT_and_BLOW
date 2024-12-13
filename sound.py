da2495041e8eb4193698e534d75968c57927be91
tagra-git <tagrana0711@gmail.com>
2024-12-13 14:44:14 +0900

    ファイル書き込み

--- sound.py
+++ sound.py
@@ -1,7 +1,9 @@
 # 音を鳴らす
 import keyboard
-import os
 import pygame
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # 共通
@@ -10,6 +12,15 @@ def play(filename):
         print(f"ファイルが見つかりません: {filename}")
         return
     pygame.mixer.init()
+    with open(r'conf\sound.txt', 'r') as f:
+        try:
+            for line in f:  # 全て表示（途中）
+                print(line, end='')
+                if not line:
+                    print(line)
+        except OSError:
+            print('ファイルの読み込み中にエラーが発生しました。')
+            pass
     pygame.mixer.music.load(filename)
     pygame.mixer.music.play()
     # print("音声を再生中...")
@@ -34,8 +45,17 @@ def volume():
     pygame.mixer.init()
     # print(f'現在の音量は{}です')
     input_volume = keyboard.input_int('音量を入力: ')
-    volume = input_volume / 100
-    pygame.mixer.music.set_volume(volume)
+    volume = str(input_volume / 100)
+    # pygame.mixer.music.set_volume(volume)  # 音量を設定
+    if not os.path.exists(r'conf\sound.txt'):
+        print("ファイルが見つかりません: ")
+        return
+    with open(r'conf\sound.txt', 'a') as f:
+        try:
+            f.write(f'{volume}\n')
+        except OSError:
+            print('ファイルの書き込み中にエラーが発生しました。')
+            pass
     print(f'音量を{input_volume}に設定しました')
     # play(r"audio\hometai.mp3")
 
