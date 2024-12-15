38b5441ac8af86ef12a4c1c0a4f7aac924cb15fd
tagra-git <tagrana0711@gmail.com>
2024-12-15 23:46:00 +0900

    説明を変数に格納

--- how_to.py
+++ how_to.py
@@ -3,10 +3,7 @@ from keyboard import input_is_q
 
 DECO = '*' * 70
 TITLE = '                            遊び方'
-
-
-def how_to():
-    info = """
+HOW_TO = """
 1. コンピュータがランダムな数字を生成します。
     例: 3桁の数字 (000～999) または 4桁の数字 (0000～9999)
 2. その数字を予想して入力してください。
@@ -17,15 +14,14 @@ def how_to():
 4. これを繰り返します。
 5. 結果が表示されます。
 """
-    print(info)
 
 
-# メイン処理
+# メイン関数
 def main():
     print(f'\n{DECO}')
     print(f'{TITLE}')
     print(f'{DECO}')
-    how_to()
+    print(HOW_TO)
     input_is_q()
     print(f'\n{DECO}\n')
 
