f1f40e339d2ff6bb4ddf7e948e5b40d7d1e13743
tagra-git <tagrana0711@gmail.com>
2024-12-16 23:49:44 +0900

    入力に関するやつ色々

--- keyboard.py
+++ keyboard.py
@@ -2,35 +2,39 @@
 
 
 # キーボードから整数を入力して返す
+# 整数以外が入力された場合は再入力させる
 def input_int(prompt: str) -> int:
     while True:
-        num = input(prompt)
-        if num.isdigit():
+        user_input = input(prompt)
+        if user_input.isdigit():
             break
         else:
             print('[エラー!!] 整数で入力してください')
 
-    return int(num)
+    return int(user_input)
 
 
 # キーボードからyまたはnを入力して返す
 # True:yが入力された場合 False:nが入力された場合
 def input_boolean(prompt: str) -> bool:
+    y_inputs = ['y', 'ｙ']
+    n_inputs = ['n', 'ｎ']
     while True:
-        str = input(f'{prompt} (y/n): ')
-        if str.lower() == 'y' or str.lower() == 'ｙ':
+        user_input = input(f'{prompt} (y/n): ').lower()
+        if user_input in y_inputs:
             return True
-        elif str.lower() == 'n' or str.lower() == 'ｎ':
+        elif user_input in n_inputs:
             return False
+        print('[エラー!!] "y" または "n" を入力してください')
 
 
 # キーボードからQを入力して返す
 # True:Qが入力された場合
 def input_is_q() -> bool:
+    q_inputs = ['q', 'ｑ']
     while True:
-        str = input('戻るには"Q"を押してください: ')
-        if (str.lower() == 'q' or str.lower() == 'Q' or
-                str.lower() == 'ｑ' or str.lower() == 'Ｑ'):
+        user_input = input('戻るには"Q"を押してください: ').lower()
+        if user_input in q_inputs:
             return True
         else:
             print('[エラー!!] 指定されたキーを入力してください')
