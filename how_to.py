49ac7c2259e2d33de065c65713fe0e286cbb7a00
tagra-git <tagrana0711@gmail.com>
2025-01-04 05:17:45 +0900

    一文字ずつ表示

--- how_to.py
+++ how_to.py
@@ -1,20 +1,26 @@
 # ゲームの説明
 from keyboard_utils import input_is_q
 from file_utils import read_all_file
+from time import sleep
 
 
 # 定数の定義
 DECO = '*' * 70
 DECO_CUT = '-' * 70
 TITLE = '                            遊び方'
+SLEEP_TIME_ONE = 0.1
+SLEEP_TIME_LINE = 0.4
 
 
 def print_how_to():
     """ ゲームの遊び方を表示する関数 """
     line_data = read_all_file(r'\conf\how_to.txt')
     for line in line_data:
-        print(line, end='')
-    print()
+        sleep(SLEEP_TIME_LINE)
+        for _, text in enumerate(line):
+            print(text, end='', flush=True)
+            sleep(SLEEP_TIME_ONE)
+        print()
 
 
 def print_info():
