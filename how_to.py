7139d69d215d1c92fba38912b7c6f5939c245919
tagra-git <tagrana0711@gmail.com>
2025-01-04 11:34:26 +0900

    音を出しながら遊び方を表示

--- how_to.py
+++ how_to.py
@@ -1,7 +1,10 @@
 # ゲームの説明
 from keyboard_utils import input_is_q
 from file_utils import read_all_file
+from sound import play_keyboard
 from time import sleep
+import asyncio
+import pygame
 
 
 # 定数の定義
@@ -12,32 +15,41 @@ SLEEP_TIME_ONE = 0.1
 SLEEP_TIME_LINE = 0.4
 
 
-def print_how_to():
+async def print_how_to(event: asyncio.Event):
     """ ゲームの遊び方を表示する関数 """
     line_data = read_all_file(r'\conf\how_to.txt')
     for line in line_data:
         sleep(SLEEP_TIME_LINE)
         for _, text in enumerate(line):
             print(text, end='', flush=True)
-            sleep(SLEEP_TIME_ONE)
+            await asyncio.sleep(SLEEP_TIME_ONE)
         print()
+    event.set()
 
 
-def print_info():
+async def print_wait_play(event: asyncio.Event):
+    """ 表示が終わるまで音を鳴らし続ける関数 """
+    while not event.is_set():
+        await play_keyboard()
+    pygame.mixer.music.stop()  # 音声を即終了させる
+
+
+async def print_info():
     """ タイトルとゲームの遊び方を表示する関数 """
     print(f'\n{DECO}')
     print(TITLE)
     print(DECO)
-    print_how_to()
+    event = asyncio.Event()
+    await asyncio.gather(print_how_to(event), print_wait_play(event))
     print(f'{DECO_CUT}\n')
 
 
-def main():
+async def main():
     """ メインの関数 """
-    print_info()
+    await print_info()
     input_is_q()  # ユーザがqを押すまで待機
     print(f'\n{DECO}\n')
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
