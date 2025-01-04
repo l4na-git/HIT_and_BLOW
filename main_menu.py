27077bb6df7bf04c96e267d5615c57e4c428e493
tagra-git <tagrana0711@gmail.com>
2025-01-05 02:04:45 +0900

    メインメニューにユーザ管理追加

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,7 @@
 
 from quiz import Quiz
 from how_to import main
+import user_menu
 from sound import set_file, delete_volume_file, volume
 from utils.keyboard_utils import input_int
 from utils import log_utils
@@ -15,9 +16,10 @@ TITLE = '                            Hit&Blow'
 # メニュー番号
 MENU_HOW_TO = 1  # 遊び方
 VOLUME_SETTING = 2  # 音量の設定
-SHOW_LOG = 3  # ログの表示
-MENU_3_DIGIT = 4  # 3桁モード
-MENU_4_DIGIT = 5  # 4桁モード
+USER_MENU = 3  # ユーザ管理
+SHOW_LOG = 4  # ログの表示
+MENU_3_DIGIT = 5  # 3桁モード
+MENU_4_DIGIT = 6  # 4桁モード
 MENU_EXIT = 9  # 終了
 
 
@@ -41,6 +43,7 @@ def print_menu():
     print(f'{DECO}\n')
     print(f'{MENU_HOW_TO}. 遊び方')
     print(f'{VOLUME_SETTING}. 音量の設定')
+    print(f'{USER_MENU}. ユーザ管理')
     print(f'{SHOW_LOG}. ログを表示')
     print(f'{MENU_3_DIGIT}. 3桁モード')
     print(f'{MENU_4_DIGIT}. 4桁モード')
@@ -54,8 +57,10 @@ async def execute_menu(menu_no):
         await main()
     elif menu_no == VOLUME_SETTING:
         volume()
+    elif menu_no == USER_MENU:
+        user_menu.execute()
     elif menu_no == SHOW_LOG:
-        log_utils.print_log(file_utils.read_log())
+        log_utils.print_log(file_utils.read_log('log_data/guest.json'))
         print('~' * 70)
     elif menu_no == MENU_3_DIGIT:
         quiz_mode3 = Quiz(digit=3, max_challenge=10)
