8537094b10647120e0403d78d3a928ce825718e5
tagra-git <tagrana0711@gmail.com>
2025-01-08 05:34:33 +0900

    やり直し。画像で背景を描画

--- display/main.py
+++ display/main.py
@@ -1,64 +1,31 @@
+# これ使いたい
 
 import pygame
 import sys
-import os
-import how_to
-
-SCREEN_SIZE = (700, 570)
-BACKGROUND_COLOR = (228, 228, 228)  # (R, G, B)
-TITLE_COLOR = (20, 33, 67)
-TITLE = "Hit & Blow"
-FONT_FILE_PATH = os.path.dirname(__file__) + r"\mgenplus-1p-regular.ttf"
-BUTTON_COLOR = (26, 93, 148)
 
 
 def main():
-    pygame.init()    # Pygameを初期化
-    screen = pygame.display.set_mode(SCREEN_SIZE)    # 画面を作成
-    pygame.display.set_caption(TITLE)    # タイトルを作成
+    pygame.init()  # 初期化
 
-    font = pygame.font.SysFont("gothicg", 110)
-    button_font = pygame.font.Font(FONT_FILE_PATH, 30)
+    back_img = pygame.image.load('images/root_back.png')  # 背景画像の読み込み
+    back_img = pygame.transform.scale(back_img, (947, 535))  # 背景画像のサイズを変更
+    rect_back = back_img.get_rect()  # 背景画像のサイズを取得
 
-    # 左上の頂点ｘ座標、左上ｙ座標、横幅（px）、高さ（px）
-    show_info_button = pygame.Rect(250, 200, 200, 65)
-    show_three_button = pygame.Rect(250, 285, 200, 65)
-    show_four_button = pygame.Rect(250, 370, 200, 65)
-
-    title = font.render(TITLE, True, TITLE_COLOR)
-    show_info_test = button_font.render("遊び方", True, BACKGROUND_COLOR)
-    show_three_test = button_font.render("3桁モード", True, BACKGROUND_COLOR)
-    show_four_test = button_font.render("4桁モード", True, BACKGROUND_COLOR)
+    screen = pygame.display.set_mode((rect_back.width, rect_back.height))  # 画面を作成
+    pygame.display.set_caption("Hit & Blow")  # タイトルを作成
 
     running = True
-    # メインループ
     while running:
-        screen.fill(BACKGROUND_COLOR)
-
-        pygame.draw.rect(screen, BUTTON_COLOR, show_info_button)
-        pygame.draw.rect(screen, BUTTON_COLOR, show_three_button)
-        pygame.draw.rect(screen, BUTTON_COLOR, show_four_button)
-
-        screen.blit(title, (150, 70))  # タイトルを描画
-        screen.blit(show_info_test, (300, 210))  # 遊び方ボタンを描画
-        screen.blit(show_three_test, (285, 295))  # 3桁モードボタンを描画
-        screen.blit(show_four_test, (285, 380))  # 4桁モードボタンを描画
+        screen.fill((255, 255, 255))
+        screen.blit(back_img, rect_back)  # 背景画像の描画
 
         pygame.display.update()  # 描画処理を実行
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # 終了イベント
+            if event.type == pygame.QUIT:  # 閉じるボタンが押されたとき
                 running = False
                 pygame.quit()  # pygameのウィンドウを閉じる
                 sys.exit()  # システム終了
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if show_info_button.collidepoint(event.pos):
-                    how_to.main()
-                if show_three_button.collidepoint(event.pos):
-                    print("play 3 digit mode")
-                if show_four_button.collidepoint(event.pos):
-                    print("play 4 digit mode")
 
 
 if __name__ == "__main__":
