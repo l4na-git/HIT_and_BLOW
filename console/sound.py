9201f017d5fb6d0e3de5412bb74d3ffcefb72810
tagra-git <tagrana0711@gmail.com>
2025-01-13 04:58:18 +0900

    update sound.py

--- console/sound.py
+++ console/sound.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import utils.keyboard_utils as keyboard
-from utils.file_utils import search_file, write_file, read_file, change_volume, read_log
+from utils.file_utils import search_file, change_volume, read_log
 import pygame
 import os
 import asyncio
@@ -11,57 +11,35 @@ from time import sleep
 
 
 # 定数の定義
-dir = os.path.dirname(__file__)
-file_path = os.path.join(os.path.dirname(__file__), '../config', 'setting.json')
+CONF_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                r'..\config\setting.json'))
 
-SEARCH_FILE_PATH = dir + r'\setting.json'
-CONF_FILE_PATH = os.path.join(dir, SEARCH_FILE_PATH)
 DEFAULT_VOLUME = 0.3
 DECO = '*' * 70
 TITLE = '                           音量の設定'
 SLEEP_TIME = 0.7
 
 
-# 共通
+def create_file_path(filename: str) -> str:
+    """ ファイルパスを作成する関数 """
+    return os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        r'..\audio\\' + filename))
+
+
 async def play(filename):
     """ 音を再生する関数 """
-    search_file(filename)
     pygame.mixer.init()
-    data_list = read_log(file_path)
+    data_list = read_log(CONF_FILE_PATH)
     volume = data_list['sound']
     pygame.mixer.music.set_volume(volume)  # 音量を設定
-    pygame.mixer.music.load(filename)
+    sound_file = create_file_path(filename)
+    pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play()
     # print("音声を再生中...")
     while pygame.mixer.music.get_busy():
         await asyncio.sleep(0.05)
         # pass
     # print("再生終了")
-
-
-async def play_correct():
-    """ 正解のときに呼び出す関数 """
-    await play(r"audio\happy.mp3")
-
-
-async def play_wrong():
-    """ 不正解のときに呼び出す関数 """
-    await play(r"audio\sad.mp3")
-
-
-async def play_count():
-    """ カウントダウンをするときに呼び出す関数 """
-    await play(r"audio\count.mp3")
-
-
-async def play_quiz():
-    """ 何回目の挑戦を表示するときに呼び出す関数 """
-    await play(r"audio\challenge.mp3")
-
-
-async def play_keyboard():
-    """ キーボードの音を鳴らす関数 """
-    await play(r"audio\keyboard.mp3")
 
 
 def volume():
@@ -74,14 +52,14 @@ def volume():
     print('[注意！] パソコンでミュートになっている場合は音がなりません。\n')
     sleep(SLEEP_TIME)
     pygame.mixer.init()
-    data_list = read_log(file_path)
+    data_list = read_log(CONF_FILE_PATH)
     volume = data_list['sound'] * 100
     print(f'現在の音量は{volume:.0f}%です')
     sleep(SLEEP_TIME)
     set_volume = keyboard.input_volume('音量を入力: ')
     volume = str(set_volume / 100)
     search_file(CONF_FILE_PATH)
-    change_volume(file_path, set_volume / 100)
+    change_volume(CONF_FILE_PATH, set_volume / 100)
     print(f'\n音量を{set_volume}%に設定しました')
     sleep(SLEEP_TIME)
     print(f'\n{DECO}\n')
@@ -91,10 +69,9 @@ def print_with_sound(prompt: str) -> None:
     """ 一文字ずつ表示しながら音を再生する """
     # 初期化
     DELAY = 0.1
-    PATH = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                        r'..\audio\keyboard.mp3'))
+    PATH = create_file_path("keyboard.mp3")
     pygame.mixer.init()
-    data_list = read_log(file_path)
+    data_list = read_log(CONF_FILE_PATH)
     volume = data_list['sound']
     pygame.mixer.music.set_volume(volume)
 
@@ -111,18 +88,8 @@ def print_with_sound(prompt: str) -> None:
     # 文字の出力が終わったら音楽を停止
     pygame.mixer.music.stop()
 
-# def set_file():
-#     """ プログラム実行時に呼び出す関数 """
-#     return write_file(f'{DEFAULT_VOLUME}', CONF_FILE_PATH)
-
-
-# def delete_volume_file():
-#     """ ファイルの削除をする関数 """
-#     delete_file(CONF_FILE_PATH)
-
 
 if __name__ == "__main__":
-    change_volume(file_path, 0.3)
+    change_volume(CONF_FILE_PATH, 0.3)
     volume()
-    asyncio.run(play_correct())
     # delete_volume_file()
