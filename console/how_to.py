8d24bab1c6dd3cdf21da378fde9c53385d0639f2
tagra-git <tagrana0711@gmail.com>
2025-01-13 02:39:49 +0900

    update how_to.py

--- console/how_to.py
+++ console/how_to.py
@@ -1,8 +1,8 @@
 # ゲームの説明
 from utils.keyboard_utils import input_is_q
 from utils.file_utils import read_all_file
+from console.sound import print_with_sound
 from time import sleep
-import asyncio
 import os
 
 
@@ -10,33 +10,32 @@ import os
 DECO = '*' * 70
 DECO_CUT = '-' * 70
 TITLE = '                            遊び方'
-SLEEP_TIME_ONE = 0.1
-SLEEP_TIME_LINE = 0.7
+SLEEP_TIME = 0.3
 CONF_FILE_PATH = os.path.join(
     os.path.dirname(__file__), '../config', 'how_to.txt')
 
 
-async def print_how_to():
+def print_how_to():
     """ ゲームの遊び方を表示する関数 """
     line_data = read_all_file(CONF_FILE_PATH)
     print('\033[?25l', end='')  # カーソル消去
     for line in line_data:
-        sleep(SLEEP_TIME_LINE)
-        print(line, end='', flush=True)
+        sleep(SLEEP_TIME)
+        print_with_sound(line)
         print()
     print('\033[?25h', end='')  # カーソル表示
 
 
-async def main():
+def main():
     """ メインの関数 """
     print(f'\n{DECO}')
     print(TITLE)
     print(DECO)
-    await print_how_to()
+    print_how_to()
     print(f'{DECO_CUT}\n')
     input_is_q()  # ユーザがqを押すまで待機
     print(f'\n{DECO}\n')
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
